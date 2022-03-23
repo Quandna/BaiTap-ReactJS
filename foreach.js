840f907a8fc3609b194d40d919b04c56223f0b1b
Quân N.Đ <89737289+Quandna@users.noreply.github.com>
2022-03-23 21:08:57 +0700

    Update foreach.js

--- foreach.js
+++ foreach.js
@@ -2,11 +2,14 @@ const fruit = ["tao","le","xoai"];
 Array.prototype.foreach1 = function(callback){
   for(let index in this) {
     if(this.hasOwnProperty(index)){
-      callback(this[index],index,this);
+      callback(this[index],index);
     }
   }
 };
 
-fruit.foreach1(function(fruit,index,array){
-  console.log(fruit,index,array);
-});
+fruit.foreach1(
+    function(fruit,index)
+  {
+  console.log(fruit,index);
+  }
+);
